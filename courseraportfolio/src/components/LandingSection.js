ac34658ffade16182c70ebf3c040e8acdeec8c53
meliance <melakmu66@gmail.com>
2024-01-01 23:15:08 +0300

    Landing section file added

---
+++ courseraportfolio/src/components/LandingSection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreen from "./FullScreen";
+
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreen
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+
+  </FullScreen>
+);
+
+export default LandingSection;
